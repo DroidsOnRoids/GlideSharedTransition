2640a054f5a0b7b8f1454a67446589fc649d9f41
nonda <nonda95@gmail.com>
2018-04-04 10:33:30 +0200

    reload image after transition ends

--- app/src/main/java/pl/droidsonrioids/glidesharedtransition/DetailActivity.kt
+++ app/src/main/java/pl/droidsonrioids/glidesharedtransition/DetailActivity.kt
@@ -2,18 +2,32 @@ package pl.droidsonrioids.glidesharedtransition
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.ChangeBounds
+import android.transition.ChangeImageTransform
+import android.transition.Transition
+import android.transition.TransitionSet
+import androidx.core.transition.doOnEnd
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
+
+    private val url by lazy {
+        intent.getStringExtra(IMAGE_URL_KEY)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         supportPostponeEnterTransition()
-        val url = intent.getStringExtra(IMAGE_URL_KEY)
         detailImage.transitionName = url
         detailImage.load(url, true) {
             supportStartPostponedEnterTransition()
         }
+        window.sharedElementEnterTransition = TransitionSet()
+                .addTransition(ChangeImageTransform())
+                .addTransition(ChangeBounds())
+                .apply {
+                    doOnEnd { detailImage.load(url) }
+                }
     }
 }
